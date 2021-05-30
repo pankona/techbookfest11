6dfd2299d9d8bb7e5d99a9869611c78335656ea1
pankona <yosuke.akatsuka@gmail.com>
2021-05-30 20:53:27 +0900

    use config-ebook.yml as default

--- build-in-docker-compose.sh
+++ build-in-docker-compose.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-[ ! -z $REVIEW_CONFIG_FILE ] || REVIEW_CONFIG_FILE=config.yml
+[ ! -z $REVIEW_CONFIG_FILE ] || REVIEW_CONFIG_FILE=config-ebook.yml
 
 docker-compose run -u docker --rm review /bin/bash -ci "cd /book && REVIEW_CONFIG_FILE=$REVIEW_CONFIG_FILE npm run pdf"
